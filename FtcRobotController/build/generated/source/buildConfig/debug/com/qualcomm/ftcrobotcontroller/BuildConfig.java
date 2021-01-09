aa8ec02f11241ff6262860fc4ad31ae729fe6a92
Mauricio Muñoz <a00832956@itesm.mx>
2021-01-09 13:03:13 -0800

    added brazo test

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -10,5 +10,5 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 38;
   public static final String VERSION_NAME = "6.0";
   // Fields from default config.
-  public static final String BUILD_TIME = "2021-01-06T17:05:59.134-0800";
+  public static final String BUILD_TIME = "2021-01-09T12:52:52.494-0800";
 }
