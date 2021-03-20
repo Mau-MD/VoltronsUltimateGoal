42ceb92211b3dff264a3f1cc74dbbb55c4423e99
Mauricio Muñoz <a00832956@itesm.mx>
2021-03-20 10:22:52 -0700

    added imu documentation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Voltrons/hardware/Imu.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Voltrons/hardware/Imu.java
@@ -12,14 +12,26 @@ public class Imu {
     private BNO055IMU imu;
 
 
+    /**
+     * Initializes imu class given an imu
+     * @param imu imu
+     */
     public Imu(BNO055IMU imu) {
         this.imu = imu;
     }
 
+    /**
+     * Gets the current heading of the robot. 180 -> 0 -> -180
+     * @return current heading
+     */
     public double getAngle() {
         return imu.getAngularOrientation(AxesReference.INTRINSIC,AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
     }
 
+    /**
+     * Gets the current heading of the robot normalized to 0 -> 360 degrees
+     * @return current heading normalized to 360 degrees
+     */
     public double getAngleNormalized() {
         double angle = imu.getAngularOrientation(AxesReference.INTRINSIC,AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
         if (angle <= 0) {
@@ -30,7 +42,12 @@ public class Imu {
         }
     }
 
-    // Error negativo significa ir a la izquierda. Positivo derecha
+    /**
+     * Gets the error between two angles
+     * @param angle current angle
+     * @param goal goal angle
+     * @return error
+     */
     public static double getError(double angle, double goal) {
         double error = goal - angle;
         while (error > 180) {
@@ -42,6 +59,11 @@ public class Imu {
         return error;
     }
 
+    /**
+     * Normalizes spline angle to the form 0 -> 360
+     * @param angle spline angle
+     * @return normalized angle
+     */
     public static double normalizeSplineAngle(double angle) {
         double newAngle = angle;
         newAngle %= 360;
